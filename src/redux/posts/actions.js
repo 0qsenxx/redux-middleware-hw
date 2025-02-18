4729c0be080019a2154d4addbaf5fb1e87e925fc
0qsenxx <semen.antipyuk@gmail.com>
2025-02-18 22:02:41 +0200

    Updates

--- src/redux/posts/actions.js
+++
@@ -1,9 +0,0 @@
-const getPosts = "getPosts";
-const searchPost = "searchPost";
-
-const searchPostAction = (value) => ({
-  type: "searchPost",
-  payload: value,
-});
-
-export { getPosts, searchPost, searchPostAction };
